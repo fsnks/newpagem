86a8ed0c106665474ccd36bd77bb986f104810d5
fsnks <130067396+fsnks@users.noreply.github.com>
2023-05-07 01:08:17 +0100

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,7 +120,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS =(os.path.join(BASE_DIR, 'static'),)
 ZEET_HTTP_PORT = 8000
-DJANGO_SETTINGS_MODULE = mysite.settings
+DJANGO_SETTINGS_MODULE = newpagem.settings
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
